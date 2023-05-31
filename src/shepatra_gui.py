0ba0a26b3ed67ae585c913bac929cfd0d84b2a25
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-05-31 18:28:41 +0900

    make MakingHashedPasswordScene()

--- src/shepatra_gui.py
+++ src/shepatra_gui.py
@@ -51,233 +51,71 @@ if Path(SCRIPT_DIR / HASHFUNC_LAYERS_RECIPES_FILENAME).exists():
     recipedict = load_recipedict_from_json(
         SCRIPT_DIR / HASHFUNC_LAYERS_RECIPES_FILENAME
     )
-
-
-def recipedict_options_for_dropdown() -> list[ft.dropdown.Option]:
-    return [
-        ft.dropdown.Option(key=recipe_name, text=recipe_name)
-        for recipe_name in recipedict.keys()
-    ]
-
-
-recipedict_dropdown = ft.Dropdown(
-    options=recipedict_options_for_dropdown(),
-    bgcolor=ft.colors.WHITE12,
-    border_color=ft.colors.WHITE,
-    expand=True,
-)
-
-new_recipe = HashFuncLayersRecipe(recipe=[], is_variable_length_algorithm_only=False)
-
 # --
 
 
-def making_hashed_password_tab(page: ft.Page) -> ft.Tab:
-    def make_password_hashed_on_enter(e):
-        if recipedict_dropdown.value is None:
-            recipedict_dropdown.error_text = navi_texts[
-                "raise_error_not_recipe_selected"
+class MakingHashedPasswordScene(ft.UserControl):
+    def __init__(self):
+        super().__init__()
+
+    def build(self):
+        self.recipe_dropdown = ft.Dropdown(
+            options=[
+                ft.dropdown.Option(key=recipe_name, text=recipe_name)
+                for recipe_name in recipedict
             ]
-            recipedict_dropdown.update()
+        )
+        self.password_textfield = ft.TextField(
+            hint_text=navi_texts["submit_with_enter"],
+            on_submit=lambda e: self.make_password_hashed(),
+        )
+        self.result_textfield = ft.TextField(
+            hint_text=navi_texts["hash_value_output_is_here"], read_only=True
+        )
+        return ft.Container(
+            content=ft.Column(
+                [self.recipe_dropdown, self.password_textfield, self.result_textfield]
+            )
+        )
+
+    def make_password_hashed(self):
+        if not self.is_recipe_selected():
             return
-        if password_inputfield.value:
-            hexdigested_password = hash_str_as_hexdigest(
-                password_inputfield.value,
+        if self.password_textfield.value:
+            hashed_password = hash_str_as_hexdigest(
+                self.password_textfield.value,
                 generate_hashfunclayers(
-                    *recipedict[recipedict_dropdown.value]["recipe"]
+                    *recipedict[self.recipe_dropdown.value]["recipe"]
                 ),
             )
-            pyperclip.copy(hexdigested_password)
-            hash_outputfield.value = hexdigested_password
-            hash_outputfield.helper_text = navi_texts["copied_to_clipboard"]
-            hash_outputfield.helper_style = ft.TextStyle(
+            pyperclip.copy(hashed_password)
+            self.result_textfield.value = hashed_password
+            self.result_textfield.helper_text = navi_texts["copied_to_clipboard"]
+            self.result_textfield.helper_style = ft.TextStyle(
                 color=ft.colors.GREEN_ACCENT_200
             )
-            hash_outputfield.update()
-            if recipedict_dropdown.error_text:
-                recipedict_dropdown.error_text = None
-                recipedict_dropdown.update()
+            self.result_textfield.update()
+            if self.recipe_dropdown.error_text:
+                self.recipe_dropdown.error_text = None
+                self.recipe_dropdown.update()
 
-    recipedict_dropdown_header = ft.Text(
-        navi_texts["which_recipe_would_you_like"] + ":"
-    )
-    password_inputfield_header = ft.Text(navi_texts["submit_with_enter"] + ":")
-    password_inputfield = ft.TextField(
-        hint_text=navi_texts["input_password"],
-        bgcolor=ft.colors.WHITE10,
-        border_color=ft.colors.WHITE,
-        expand=True,
-        on_submit=make_password_hashed_on_enter,
-    )
-    hash_outputfield = ft.TextField(
-        hint_text=navi_texts["hash_value_output_is_here"],
-        read_only=True,
-        multiline=True,
-        expand=True,
-    )
-    return ft.Tab(
-        text=navi_texts["make_hashed_password"],
-        content=ft.Column(
-            [
-                ft.Column(
-                    [
-                        ft.Row(
-                            controls=[
-                                recipedict_dropdown_header,
-                                recipedict_dropdown,
-                            ],
-                        ),
-                        ft.Row(
-                            controls=[
-                                password_inputfield_header,
-                                password_inputfield,
-                            ],
-                        ),
-                    ],
-                    alignment=ft.MainAxisAlignment.END,
-                ),
-                hash_outputfield,
-            ],
-        ),
-    )
-
-
-def making_recipe_tab(page: ft.Page) -> ft.Tab:
-    def add_algorithm_on_click(e):
-        if algorithm_dropdown.value is None:
-            algorithm_dropdown.error_text = navi_texts["error_no_algorithm_selected"]
-            algorithm_dropdown.update()
-            return
+    def is_recipe_selected(self) -> bool:
+        if self.recipe_dropdown.value is None:
+            self.recipe_dropdown.error_text = navi_texts[
+                "raise_error_not_recipe_selected"
+            ]
+            self.recipe_dropdown.update()
+            return False
         else:
-            algorithm_dropdown.error_text = None
-            algorithm_dropdown.update()
-        new_recipe["recipe"].append(HashFuncName[algorithm_dropdown.value])
-        recipelist_item = ft.TextButton((algorithm_dropdown.value))
-
-        def remove_recipelist_item(e):
-            listview_for_recipe.controls.remove(recipelist_item)
-            new_recipe["recipe"].remove(HashFuncName[recipelist_item.text])
-            listview_for_recipe.update()
-
-        recipelist_item.on_click = remove_recipelist_item
-        listview_for_recipe.controls.append(recipelist_item)
-        listview_for_recipe.update()
-
-    def recipe_confirmed_on_click(e):
-        print(recipe_name_inputfield.value)
-        if recipe_name_inputfield.value:
-            recipedict[recipe_name_inputfield.value] = new_recipe
-            store_recipedict_to_json(
-                SCRIPT_DIR / HASHFUNC_LAYERS_RECIPES_FILENAME,
-                recipedict,
-            )
-            recipedict_dropdown.options = recipedict_options_for_dropdown()
-            recipedict_dropdown.update()
-            update_to_recipe_confirmed_dialog(submit_dialog)
-            page.update()
-
-    recipe_name_inputfield = ft.TextField(
-        hint_text=navi_texts["name_of_recipe"], on_submit=recipe_confirmed_on_click
-    )
-
-    def make_submit_dialog() -> ft.AlertDialog:
-        return ft.AlertDialog(
-            title=ft.Text(navi_texts["give_name_to_recipe"]),
-            actions=[
-                recipe_name_inputfield,
-                ft.ElevatedButton(
-                    navi_texts["submit"],
-                    expand=True,
-                    on_click=recipe_confirmed_on_click,
-                ),
-            ],
-        )
-
-    def update_to_submit_dialog(dialog: ft.AlertDialog):
-        dialog = make_submit_dialog()
-        dialog.update
-
-    submit_dialog = make_submit_dialog()
-
-    def open_submit_dialog_on_click(e):
-        if new_recipe["recipe"]:
-            page.dialog = submit_dialog
-            submit_dialog.open = True
-            page.update()
-
-    def close_submit_dialog_on_click(e):
-        submit_dialog.open = False
-        update_to_submit_dialog(submit_dialog)
-        page.update()
-
-    def update_to_recipe_confirmed_dialog(dialog: ft.AlertDialog) -> ft.AlertDialog:
-        dialog.title = ft.Text(
-            navi_texts["recipe_generated"], color=ft.colors.GREEN_ACCENT_200
-        )
-        dialog.actions = [
-            ft.ElevatedButton(
-                navi_texts["close"],
-                expand=True,
-                on_click=close_submit_dialog_on_click,
-            ),
-        ]
-        dialog.update()
-
-    algorithm_dropdown = ft.Dropdown(
-        options=[
-            ft.dropdown.Option(hashfuncname.name) for hashfuncname in HashFuncName
-        ],
-        expand=True,
-        bgcolor=ft.colors.WHITE12,
-        border_color=ft.colors.WHITE,
-        hint_text=navi_texts["select_algorithm"],
-    )
-    btn_to_add_algorithm = ft.IconButton(
-        icon=ft.icons.ADD,
-        icon_color="blue400",
-        tooltip=navi_texts["add_algorithm"],
-        on_click=add_algorithm_on_click,
-    )
-    btn_to_submit = ft.ElevatedButton(
-        navi_texts["submit_recipe"], on_click=open_submit_dialog_on_click
-    )
-    listview_for_recipe = ft.ListView(
-        height=200,
-        auto_scroll=True,
-    )
-    return ft.Tab(
-        text=navi_texts["go_to_recipe_making"],
-        content=ft.Column(
-            [
-                ft.Row(
-                    [
-                        algorithm_dropdown,
-                        btn_to_add_algorithm,
-                    ]
-                ),
-                listview_for_recipe,
-                btn_to_submit,
-            ],
-        ),
-    )
+            return True
 
 
 def main(page: ft.Page):
     # -configure window-
     page.title = "Shepatra"
-    page.window_width = 522
-    page.window_height = 440
-    page.window_resizable = False
     # --
 
-    tabs = ft.Tabs(
-        indicator_padding=11,
-        selected_index=1,
-        tabs=[making_hashed_password_tab(page), making_recipe_tab(page)],
-        expand=1,
-    )
-
-    page.add(tabs)
+    page.add(MakingHashedPasswordScene())
     page.update()
 
 
